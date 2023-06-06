d9521ac06d4294d1812894a48e7a50259ffdeeb0
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-06 18:09:17 +0000

    correct negative feature

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    number = number % 10
     if number < 0:
         number = number * -1
+        number = number % 10
         print(number, end='')
     else:
+        number = number % 10
         print(number, end='')
     return (number)
