216c910a5152ae449243fb5cfdf49d4d2c0a696f
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-06 19:12:06 +0000

    changed algorithm to print alphabets in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if (i % 2) == 0:
-        print((chr(i)), end='')
-    else:
-        print((chr(i - 32)), end='')
+for i in range(122, 96, -2):
+    print("{}{}".format(chr(i), chr((i - 1) - 32)), end='')
