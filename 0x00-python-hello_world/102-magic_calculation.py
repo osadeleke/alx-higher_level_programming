71f3ff1b62a85cef2e46b879fe3cf3386f418d0f
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-07 19:14:58 +0000

    add program to read bytecode

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -2,5 +2,10 @@
 import dis
 
 
-def magic_calculation(a, b):
-    return (98 + a ** b)
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return a * b - c
