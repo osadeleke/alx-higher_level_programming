642d0c38d8a1632bc7db7c7f3a664b8e38e5be1b
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-07 15:42:04 +0000

    remove error test

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 10):
-    # print(random)
-    # print anything
     for j in range(0, 10):
         if i != j and j > i and i < 8:
             print("{}{}, ".format(i, j), end='')
