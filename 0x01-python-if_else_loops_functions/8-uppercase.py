a84e75442a35415cf9ba5cdb8bf3953139159d0e
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-07 11:13:53 +0000

    correct uppercase task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,10 +3,8 @@ def uppercase(str):
     for char in str:
         if 'a' <= char <= 'z':
             char = chr(ord(char) - 32)
-            #print("{}".format(chr(ord(char) - 32)), end='')
         else:
             char = char
-            #print("{}".format(char), end='')
         print("{}".format(char), end='')
     print("")
 
