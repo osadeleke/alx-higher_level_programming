0357d7859aae6e8fd2fe5a95a9ed951790fcc81f
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-08 15:11:58 +0000

    fix import error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0 as add
 if __name__ == "__main__":
+    import add_0 as plus
     a = 1
     b = 2
-    c = add.add(a, b)
+    c = plus.add(a, b)
     print("{} + {} = {}".format(a, b, c))
