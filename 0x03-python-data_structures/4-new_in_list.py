b6fd4b527f0b77f500c74b7d6b549f81a0037252
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-11 15:55:06 +0000

    add program to copy new list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        n_list = my_list
+        n_list = []
+        for i in range(len(my_list)):
+            n_list = n_list + [my_list[i]]
         return n_list
     else:
         n_list = []
