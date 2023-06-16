a049bb76a7f289559d789c38ce89224fae9682d4
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-16 15:28:53 +0000

    add program to print all integers in a list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    i = 0
-    for list in my_list:
+    for i in range(len(my_list)):
         print("{}".format(my_list[i]))
-        i = i + 1
