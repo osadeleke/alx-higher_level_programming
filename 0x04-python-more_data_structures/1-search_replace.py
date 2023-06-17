3bfe3ddc444f9de840fd9893bfd218b2ec8d24e7
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-17 17:38:39 +0000

    add program to replace element in list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list is not None:
+        new_list = []
+        for element in my_list:
+            new_list.append(element)
+        i = 0
+        while True:
+            try:
+                i = my_list.index(search, i + 1)
+                new_list[i] = replace
+            except ValueError:
+                return new_list
