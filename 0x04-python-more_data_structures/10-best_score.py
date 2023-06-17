1feacba9cc1c17fb501246bf74af2ac374ba090f
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-17 19:00:32 +0000

    fixed feature for empty dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary == {}:
+        return None
+    elif a_dictionary is not None:
         sort_scores = sorted(a_dictionary.values())
         i = sort_scores[len(sort_scores) - 1]
         for key, value in a_dictionary.items():
