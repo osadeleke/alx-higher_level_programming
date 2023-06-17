10e35fc46f341277b145a0049d7ce87315e0e2e2
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-17 18:57:52 +0000

    add program that finds highest values and prints key

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        sort_scores = sorted(a_dictionary.values())
+        i = sort_scores[len(sort_scores) - 1]
+        for key, value in a_dictionary.items():
+            if value == i:
+                return key
+    else:
+        return None
