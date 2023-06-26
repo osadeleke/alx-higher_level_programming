182405ba872085fe367b0a667a7b8a95e01e1ea9
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-26 08:42:52 +0000

    add program to print list ignoring exception

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        i = 0
+        for j in range(x):
+            print("{}".format(my_list[i]), end="")
+            i = i + 1;
+    except:
+        pass
+    finally:
+        print()
+        return i
