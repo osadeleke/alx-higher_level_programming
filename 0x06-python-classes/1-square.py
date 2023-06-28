33465abe587baafcf22ac3353df5d0a70b0b622e
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-28 15:17:32 +0000

    add program to make value private

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class named square with private instance"""
+
+
+class Square:
+    """Contains a private instance attribute"""
+    def __init__(self, size):
+        """Function to initialize size"""
+        self.__size = size
