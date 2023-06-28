4b361a33afda4e6674c3a16fc13b54d158a0fa94
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-28 16:49:51 +0000

    add program to print square with #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Defines a class named square with private instance"""
+
+
+class Square:
+    """Contains a private instance attribute"""
+    def __init__(self, size=0):
+        """Method to initialize size"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter method for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        if type(value) is not int:
+            print("size must be an integer", end="")
+            raise TypeError
+        elif value < 0:
+            print("size must be >= 0", end="")
+            raise ValueError
+        else:
+            self.__size = value
+
+    def area(self):
+        """Method to return area of square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """prints the square using #"""
+        if self.__size != 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
+        else:
+            print()
