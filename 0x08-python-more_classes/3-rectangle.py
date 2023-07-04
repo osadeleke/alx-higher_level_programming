0e4249d4b38dd44472abf47ee2cf165bc447ae9d
Olusegun Adeleke <oadeleke123@gmail.com>
2023-07-04 17:03:54 +0000

    fix return for either width or height equals 0

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -73,6 +73,8 @@ class Rectangle:
             return (2 * (self.__width + self.__height))
 
     def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ''
         img = ""
         for i in range(self.__height):
             for j in range(self.__width):
