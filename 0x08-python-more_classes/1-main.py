098fe2ac58c4d4317fbe59d2842263e85f644327
Olusegun Adeleke <oadeleke123@gmail.com>
2023-07-04 15:40:10 +0000

    fix input width and height not int or 0 and above

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -7,3 +7,8 @@ print(my_rectangle.__dict__)
 my_rectangle.width = 10
 my_rectangle.height = 3
 print(my_rectangle.__dict__)
+
+try:
+    my_rectangle = Rectangle(2, -3)
+except Exception as e:
+    print("[{}]{}".format(e, e))
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,7 +9,16 @@ class Rectangle:
     This is a rectangle
     """
     def __init__(self, width=0, height=0):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         self.__width = width
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__height = height
 
     @property
