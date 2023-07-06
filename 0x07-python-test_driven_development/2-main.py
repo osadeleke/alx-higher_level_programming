75f14fbc4953198afcd87f6f448210105e3a6eb3
Olusegun Adeleke <oadeleke123@gmail.com>
2023-07-06 19:41:23 +0000

    fix test

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -2,8 +2,10 @@
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 matrix = [
-    [1, "b", 3],
-    [4, 5, 6]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8],
+    [10, 11, 13]
 ]
 print(matrix_divided(matrix, 3))
 print(matrix)
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,6 +13,7 @@ def matrix_divided(matrix, div):
     if isinstance(div, int) == 0 and isinstance(div, float) == 0:
         raise TypeError("div must be a number")
     new_matrix = []
+    i = 0
     for row in matrix:
         temp = []
         for element in row:
@@ -22,5 +23,11 @@ def matrix_divided(matrix, div):
                 s = "matrix must be a matrix (list of lists)"
                 n = " of integers/floats"
                 raise TypeError(s + n)
+        if i != 0:
+            if i != len(row):
+                s = "Each row of the matrix"
+                n = " must have the same size"
+                raise TypeError(s + n)
+        i = len(row)
         new_matrix.append(temp)
     return new_matrix
