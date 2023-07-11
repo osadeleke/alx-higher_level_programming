5d4001a9838f14a489fba07169dbcab0f1bcc7ee
Olusegun Adeleke <oadeleke123@gmail.com>
2023-07-11 18:12:07 +0000

    fix: pycodestyle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,6 +10,7 @@ class BaseGeometry:
         """
         check area"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """validates value
         """
@@ -17,6 +18,7 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
 
 class Rectangle(BaseGeometry):
     """inherits from BaseGeometry
