1ee9c5b1a20e95aae816ec0a4d4c8da6191b7923
Olusegun Adeleke <oadeleke123@gmail.com>
2023-07-18 13:40:41 +0000

    add: program to write into a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Writes a string to a text file (UTF-8) and returns the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file
+    Return: number of characters written
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
