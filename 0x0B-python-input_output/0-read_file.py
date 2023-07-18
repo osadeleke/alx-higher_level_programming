ada7693243bf58323cf7c1716917292cc93c83e3
Olusegun Adeleke <oadeleke123@gmail.com>
2023-07-18 13:34:38 +0000

    add: program to read from file and print to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module reads a text file (UTF-8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    Function reads a text file (UTF-8) and prints it to stdout
+    """
+    with open(filename) as f:
+        cont = f.read()
+        print(cont)
