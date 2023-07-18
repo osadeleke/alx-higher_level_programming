4bc205ebdf4a54f7b5d0eeb43561837e0f509450
Olusegun Adeleke <oadeleke123@gmail.com>
2023-07-18 14:01:56 +0000

    add: program to return JSON representation of an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Returns JSON representation of an object (string)
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns JSON representation of an object (string)
+    """
+    return json.dumps(my_obj)
