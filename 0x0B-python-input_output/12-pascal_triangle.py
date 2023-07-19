9342be5d6e83eb3f93ca8bb21ca1a6a7a788013b
Olusegun Adeleke <oadeleke123@gmail.com>
2023-07-19 05:15:27 +0000

    add: pascal triangle program

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+List of List of integers representing pascal's triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    List of list of integers rep pascal's triangle
+    """
+    if n <= 0:
+        main_list = []
+    elif n == 1:
+        main_list = [1]
+    elif n == 2:
+        main_list = [1, 1]
+    else:
+        main_list = [[1], [1, 1]]
+        for i in range(3, n+1):
+            m_list = main_list[i - 2]
+            tmp_list = [1]
+            k = 0
+            for j in range(1, i-1):
+                tmp_list.append(m_list[k] + m_list[k+1])
+                k = k + 1
+            tmp_list.append(1)
+            main_list.append(tmp_list)
+    return main_list
