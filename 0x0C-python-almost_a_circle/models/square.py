1573e409174f7d5e2ca99848e87108af045670dc
Olusegun Adeleke <oadeleke123@gmail.com>
2023-07-20 18:20:12 +0000

    add: feature to update size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -187,6 +187,31 @@ class Square(Rectangle):
         self.width = size
         self.height = size
 
+    def update(self, *args, **kwargs):
+        """
+        To update shape
+        """
+        for i, arg in enumerate(args):
+            if i == 0:
+                self.id = arg
+            if i == 1:
+                self.__size = arg
+                self.width = self.__size
+            if i == 2:
+                self.x = arg
+            if i == 3:
+                self.y = arg
+        if kwargs:
+            if "size" in kwargs:
+                self.__size = kwargs["size"]
+                self.width = self.__size
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
     def __str__(self):
         paone = "[Square] ({}) {}/".format(self.id, self.x)
         patwo = "{} - {}".format(self.y, self.width)
