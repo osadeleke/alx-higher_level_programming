8a544e12aa9d988a70a93c44b1e74b4a2411215f
Olusegun Adeleke <oadeleke123@gmail.com>
2023-07-20 00:55:48 +0000

    add: program to track instances spunned

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Base for other classes in the project
+"""
+
+
+class Base:
+    """
+    Base for other classes in the project
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
