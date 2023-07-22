7d1b5cd598beefb2e79b28fa7cb3e4fe40cd7061
Olusegun Adeleke <oadeleke123@gmail.com>
2023-07-22 01:52:50 +0000

    add: program to write JSON representation of a list of file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,3 +27,21 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Save Json to file
+        """
+        name = cls.__name__ + ".json"
+        old_list = []
+        for val in list_objs:
+            ins_dict = {}
+            for key, value in vars(val).items():
+                if key.startswith("_{}_".format(cls.__name__)):
+                    key = key[len(cls.__name__) + 3:]
+                ins_dict[key] = value
+            old_list.append(ins_dict)
+        json_string = cls.to_json_string(old_list)
+        with open(name, 'w') as f:
+            f.write(json_string)
