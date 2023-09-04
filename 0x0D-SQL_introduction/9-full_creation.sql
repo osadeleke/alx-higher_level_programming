f7a46f5c519cb7ddb8955065bb82f2d0bab046fd
Segun Adeleke <oadeleke123@gmail.com>
2023-09-04 18:55:18 +0100

    Add: create a table and multiple rows

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- creates a table in database in server and rows
+CREATE TABLE IF NOT EXISTS `second_table` (
+	`id` int,
+	`name` varchar(256),
+	`score` int
+	);
+
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES
+	('1', 'John', '10'),
+	('2', 'Alex', '3'),
+	('3', 'Bob', '14'),
+	('4', 'George', '8');
