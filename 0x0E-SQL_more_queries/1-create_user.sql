acbc70328af8d91cc94be5c76cce4d7d6553665c
Segun Adeleke <oadeleke123@gmail.com>
2023-09-05 23:55:21 +0100

    Fix: character error

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates a MySQL server user
-CREATE USER IF NOT EXISTS 'user_od_1'@'localhost'
-IDENTIFIED BY 'user_od_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
