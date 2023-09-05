7aa7f26645802af3062567863d6f85b873633c77
Segun Adeleke <oadeleke123@gmail.com>
2023-09-06 00:21:42 +0100

    Add: create table in database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,4 +2,6 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 USE `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `states` (
-	id int 
+	id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	name varchar(256) NOT NULL
+);
