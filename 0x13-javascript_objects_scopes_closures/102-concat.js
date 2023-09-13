e8cd7e149ac8e4993570de54e7afab05181edea3
Segun Adeleke <oadeleke123@gmail.com>
2023-09-13 13:56:10 +0100

    Add & Fix: concat files + semistandard

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,27 +2,26 @@
 
 const file = require('fs');
 
-filePathA = 'fileA';
-filePathB = 'fileB';
-filePathC = 'fileC';
+const filePathA = 'fileA';
+const filePathB = 'fileB';
+const filePathC = 'fileC';
 
 file.readFile(filePathA, 'utf8', (err, dataA) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-	file.readFile(filePathB, 'utf8', (err, dataB) => {
-		if (err) {
-			console.error(err);
-			return;
-		}
-		const dataConCat = dataA + dataB;
-		file.writeFile(filePathC, dataConCat, (err) => {
-			if (err) {
-				console.error(err);
-				return;
-			}
-		});
-	});
+  file.readFile(filePathB, 'utf8', (err, dataB) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const dataConCat = dataA + dataB;
+    file.writeFile(filePathC, dataConCat, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
 });
