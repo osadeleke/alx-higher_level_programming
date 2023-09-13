f3992c278abe1d7ec7add3121f264c3fe27c595c
Segun Adeleke <oadeleke123@gmail.com>
2023-09-13 01:56:55 +0100

    Add: number of occrences in a list

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 
-exports.nbOccurences = function (list, searchElement) => {
-	let count = 0;
-	let ind = 0;
-	while (list[ind]) {
-		if (list[ind] === searchElement) {
-			count++;
-		}
-		ind++;
-	}
-	return count;
-}
-
-module.exports = exports.nbOccurences;
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  let ind = 0;
+  while (list[ind]) {
+    if (list[ind] === searchElement) {
+      count++;
+    }
+    ind++;
+  }
+  return count;
+};
