bd44475a0ae732b9b02c0e3537ec9d9c0248dec4
Segun Adeleke <oadeleke123@gmail.com>
2023-09-13 09:28:16 +0100

    Fix: when array is empty

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
+  if (list.length === 0) {
+    return [];
+  }
   const newList = [list[list.length - 1]];
   let i;
   for (i = list.length - 2; i > -1; i--) {
---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+
+exports.logMe = function (item) {
+	static count = 0
