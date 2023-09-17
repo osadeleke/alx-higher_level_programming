cb82dfadf3b8c75a2f2227a87a9e7e631ddff215
Segun Adeleke <oadeleke123@gmail.com>
2023-09-17 08:11:57 +0100

    Add: get genres of movies titled dExter

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- get genres of movie titled dexter
+SELECT name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE title = 'Dexter';
