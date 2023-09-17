105ae2930769b951aba01c7fed0e57810a07e686
Segun Adeleke <oadeleke123@gmail.com>
2023-09-17 08:25:42 +0100

    Fix: 14 genre name ordering. Add: list all shows with genre linked

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,4 +3,5 @@ SELECT name
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE title = 'Dexter';
+WHERE title = 'Dexter'
+ORDER BY name ASC;
