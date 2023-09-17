04175b784993078b1353238455e2cefad214ae90
Segun Adeleke <oadeleke123@gmail.com>
2023-09-17 08:04:33 +0100

    Add: sort comedy genres from database

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- joining databases
+SELECT title
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE name = 'Comedy'
+ORDER BY title ASC;
