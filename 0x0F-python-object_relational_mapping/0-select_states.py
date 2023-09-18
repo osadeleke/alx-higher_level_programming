17f9b8e6dfd33f1f1e5bf876c84ae983188fe493
Segun Adeleke <oadeleke123@gmail.com>
2023-09-18 03:14:59 +0100

    Fix: add port

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,10 +4,11 @@ import sys
 
 def main():
     MY_HOST = 'localhost'
+    MY_PORT = 3306
     MY_USER = sys.argv[1]
     MY_PASS = sys.argv[2]
     MY_DB = sys.argv[3]
-    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+    db = MySQLdb.connect(host=MY_HOST, port=MY_PORT, user=MY_USER, passwd=MY_PASS, db=MY_DB)
     cur = db.cursor()
     cur.execute("SELECT id, name FROM states ORDER BY id ASC")
     rows = cur.fetchall()
