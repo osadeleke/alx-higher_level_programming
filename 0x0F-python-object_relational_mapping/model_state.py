0d532b0abdfae52e3f5e31ac45aca25786317964
Segun Adeleke <oadeleke123@gmail.com>
2023-09-18 05:15:19 +0100

    Add: mysqlalchemy database use. create table

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+"""Class definition for State and instance Base
+"""
+#import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+class State(Base):
+    """Class definition for State and instance Base
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
