8d380b4ea9b8304d315b3f381c2ba27c71d31b7d
Segun Adeleke <oadeleke123@gmail.com>
2023-09-24 17:14:28 +0100

    Fix: case sensitivity using binary key

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -23,9 +23,9 @@ def main():
             db=MY_DB
     )
     cur = db.cursor()
-    firstQuery = "SELECT id, name FROM states WHERE name"
-    secondQuery = "COLLATE utf8_bin LIKE 'N%' ORDER BY id ASC"
-    finalQuery = f"{firstQuery} {secondQuery}"
+    firstQuery = "SELECT id, name FROM states WHERE name "
+    secondQuery = "LIKE BINARY 'N%' ORDER BY id ASC"
+    finalQuery = firstQuery + secondQuery
     cur.execute(finalQuery)
     rows = cur.fetchall()
     for row in rows:
