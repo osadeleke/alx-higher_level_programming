4777a24cc23fe3a8d240a568e50b203886aaf1b2
Segun Adeleke <oadeleke123@gmail.com>
2023-09-24 18:19:25 +0100

    Add: filter database

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Print states with a filter in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    """Print states with a filter in database
+    """
+    us = sys.argv[1]
+    pa = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(us, pa, db),
+            pool_pre_ping=True
+            )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    statesFiltered = session.query(State).filter(State.name.like('%a%')).all()
+    if statesFiltered:
+        for state in statesFiltered:
+            print('{}: {}'.format(state.id, state.name))
+    else:
+        print('Nothing')
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
