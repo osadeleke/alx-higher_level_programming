3f22e661e767d31f2bab1bf259db0deb1155f359
Segun Adeleke <oadeleke123@gmail.com>
2023-10-01 00:33:31 +0100

    Add: takes in a URL, sends a request to the URL and handles exception

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8).
+
+Manage urllib.error.HTTPError exceptions and
+print: Error code: followed by the HTTP status code
+"""
+import sys
+import urllib.request
+import urllib.error
+
+
+def main():
+    """Takes in a URL, sends a request to the URL and
+    displays the body of the response (decoded in utf-8).
+    Manage urllib.error.HTTPError exceptions and
+    print: Error code: followed by the HTTP status code
+    """
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            this_page = response.read().decode('utf-8')
+            print(this_page)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}"format(e.code))
+
+
+if __name__ == "__main__":
+    main()
