c93c03bc4f79f47b5734d3c8c40a35c4eca1eef7
Segun Adeleke <oadeleke123@gmail.com>
2023-10-01 00:35:59 +0100

    Fix: Documentation issue"

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Takes in a URL, sends a request to the URL and
 displays the body of the response (decoded in utf-8).
-
 Manage urllib.error.HTTPError exceptions and
 print: Error code: followed by the HTTP status code
 """
