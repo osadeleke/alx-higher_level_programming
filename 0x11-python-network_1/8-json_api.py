0d1aef3540643d642eaff8e0189692b217f53800
Segun Adeleke <oadeleke123@gmail.com>
2023-10-01 14:55:31 +0100

    Add: get value from url dict

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter
+as a parameter.
+"""
+import requests
+import sys
+
+
+def main():
+    """takes in a letter and sends a POST request
+    to http://0.0.0.0:5000/search_user with the letter
+    as a parameter.
+    """
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) < 2:
+        data_load = {'q': ""}
+    else:
+        data_load = {'q': sys.argv[1]}
+
+    r = requests.post(url, data_load)
+    r_dumped = r.json()
+
+    if not isinstance(r_dumped, dict):
+        print("Not a valid JSON")
+        return
+    if len(r_dumped) == 0:
+        print("No result")
+        return
+
+    print("[{}] {}".format(r_dumped['id'], r_dumped['name']))
+
+
+if __name__ == "__main__":
+    main()
