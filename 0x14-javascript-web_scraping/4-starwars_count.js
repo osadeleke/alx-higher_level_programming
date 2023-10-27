71447fc6a216326cbff99e491c3883797930e1ba
Segun Adeleke <oadeleke123@gmail.com>
2023-10-27 01:11:01 +0100

    Fix: url input and use

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,13 +3,15 @@
 const request = require('request');
 
 const url = process.argv[2];
+const compStringSlice = url.slice(0, -5);
+const compString = compStringSlice.concat('people/18/');
 
 request(url, (error, response, body) => {
   const data = JSON.parse(body);
   let count = 0;
   for (const elements of data.results) {
     for (const newElements of elements.characters) {
-      if (newElements === 'https://swapi-api.alx-tools.com/api/people/18/') {
+      if (newElements === compString) {
         count = count + 1;
       }
     }
